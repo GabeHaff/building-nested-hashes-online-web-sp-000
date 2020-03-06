5c24a7eb36068e15e788229988aa17053d3a09fd
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-03-06 16:22:16 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -29,11 +29,12 @@ epic_tragedy{[:montague]
 [:status] = "dead"
 }
 
-epic_tragedy[:capulet]
+epic_tragedy{[:capulet]
 [:heroine]
 [:name]
 [:age]
 [:status] = "dead"
+}
 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
